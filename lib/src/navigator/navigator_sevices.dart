6c08c4c0d6f7fe735da24c131eda65cbd1b168a5
Haqim Haslee <haqimhaslee26@gmail.com>
2024-07-02 17:26:48 +0800

    Minor update -UI in Search page -AI Model config

--- lib/src/navigator/navigator_sevices.dart
+++ lib/src/navigator/navigator_sevices.dart
@@ -71,7 +71,7 @@ class _NavigatorServicesState extends State<NavigatorServices> {
               selectedIcon: Icon(Icons.search),
             ),
             const NavigationDrawerDestination(
-              label: Text("Ask our [AI]"),
+              label: Text("Ask AI"),
               icon: Icon(Icons.hub_outlined),
               selectedIcon: Icon(Icons.hub_rounded),
             ),
@@ -110,7 +110,7 @@ class _NavigatorServicesState extends State<NavigatorServices> {
                           selectedIcon: Icon(Icons.search),
                         ),
                         const NavigationDrawerDestination(
-                          label: Text("Ask our [AI]"),
+                          label: Text("Ask AI"),
                           icon: Icon(Icons.hub_outlined),
                           selectedIcon: Icon(Icons.hub_rounded),
                         ),
@@ -156,91 +156,5 @@ class _NavigatorServicesState extends State<NavigatorServices> {
             ),
           ],
         ));
-  }
-}
-
-class NavigationDrawerElement extends StatefulWidget {
-  const NavigationDrawerElement({super.key});
-  @override
-  State<NavigationDrawerElement> createState() =>
-      _NavigationDrawerElementState();
-}
-
-class _NavigationDrawerElementState extends State<NavigationDrawerElement> {
-  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
-  var _selectedIndex = 0;
-  // ignore: unused_field
-  final List<Widget> _windgetOption = <Widget>[
-    const SearchInventory(),
-    const AskOurAi(),
-    const About(),
-  ];
-  void openDrawer() {
-    scaffoldKey.currentState!.openDrawer();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return NavigationDrawer(
-      surfaceTintColor: Theme.of(context).colorScheme.surface,
-      backgroundColor: Theme.of(context).colorScheme.surface,
-      shadowColor: Theme.of(context).colorScheme.surface,
-      onDestinationSelected: (i) => setState(() => _selectedIndex = i),
-      selectedIndex: _selectedIndex,
-      children: <Widget>[
-        Padding(
-          padding: const EdgeInsets.fromLTRB(28, 0, 16, 10),
-          child: Text(
-            ' ',
-            style: Theme.of(context).textTheme.titleSmall,
-          ),
-        ),
-        const NavigationDrawerDestination(
-          label: Text("Parcel Tracker"),
-          icon: Icon(Icons.search),
-          selectedIcon: Icon(Icons.search),
-        ),
-        const NavigationDrawerDestination(
-          label: Text("Ask our [AI]"),
-          icon: Icon(Icons.hub_outlined),
-          selectedIcon: Icon(Icons.hub_rounded),
-        ),
-        const NavigationDrawerDestination(
-          label: Text("About"),
-          icon: Icon(Icons.info_outline),
-          selectedIcon: Icon(Icons.info_rounded),
-        ),
-        SizedBox.fromSize(
-          size: const Size(10, 55),
-          child: ClipRRect(
-            borderRadius: const BorderRadius.all(
-              Radius.circular(30),
-            ),
-            child: Material(
-              color: const Color.fromARGB(0, 255, 193, 7),
-              child: InkWell(
-                onTap: () => showLicensePage(
-                  context: context,
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      "Licenses",
-                      style: TextStyle(
-                        fontWeight: FontWeight.w500,
-                        fontSize: 14,
-                        color:
-                            Theme.of(context).colorScheme.onSecondaryContainer,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-          ),
-        ),
-      ],
-    );
   }
 }
